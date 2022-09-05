e0b92e0f7883f3f446c8d6e9f9062f03f0022692
samaneh <samane.ahmadloo2525@gmail.com>
2022-09-05 03:51:14 -0700

    Initial Commit

--- loginRegister/urls.py
+++ loginRegister/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import login_register_page,plans_page
+from .views import login_page,plans_page,register_page,forget_page
 
 
 urlpatterns = [
-    path('login-register', login_register_page, name='login_register_page'),
+    path('login', login_page, name='login_page'),
+    path('register', register_page, name='register_page'),
+    path('forget', forget_page, name='forget_page'),
     path('plans', plans_page, name='plans_page'),
 
 ]
--- loginRegister/views.py
+++ loginRegister/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
-
-def login_register_page(request):
-    return render(request, 'loginRegister/login_register.html')
-
+def login_page(request):
+    return render(request, 'loginRegister/login.html')
+def register_page(request):
+    return render(request, 'loginRegister/register.html')
+def forget_page(request):
+    return render(request, 'loginRegister/forget.html')
 def plans_page(request):
     return render(request, 'loginRegister/plans.html')
